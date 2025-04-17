faf80267f3d107d0d5080f5313ce6b27f22ae14d
Yuvraj Nirwan <yuvichauhan2000@gmail.com>
2025-04-17 17:03:56 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,37 +1,39 @@
 import React from "react";
 import "./Footer.css";
 import { useNavigate } from "react-router-dom";
-import { goAbout } from "./GlobalFunctions";
-import { goHome } from "./GlobalFunctions";
-import { goFAQ } from "./GlobalFunctions";
-import { goFeatures } from "./GlobalFunctions";
+
 function Footer() {
   const navigate = useNavigate();
+
+  const goHome = () => navigate("/home");
+  const goFeatures = () => navigate("/features");
+  const goFAQ = () => navigate("/faq");
+  const goAbout = () => navigate("/about");
+
   return (
-   
     <footer className="footer-dark text-white bg-dark mt-auto py-3 px-4 d-flex flex-wrap justify-content-between align-items-center">
       <p className="col-md-4 mb-0 footBrand">&copy; {new Date().getFullYear()} Payzz.com.</p>
 
       <ul className="nav col-md-4 justify-content-end">
         <li className="nav-item footLinks">
-          <a href="#" className="nav-link px-2 text-white" onClick={(e) => goHome(navigate, e)}>
+          <button className="nav-link px-2 text-white" onClick={goHome}>
             Home
-          </a>
+          </button>
         </li>
         <li className="nav-item footLinks">
-          <a href="#" className="nav-link px-2 text-white" onClick={(e) => goFeatures(navigate, e)}>
+          <button className="nav-link px-2 text-white" onClick={goFeatures}>
             Features
-          </a>
+          </button>
         </li>
         <li className="nav-item footLinks">
-          <a href="#" className="nav-link px-2 text-white" onClick={(e) => goFAQ(navigate, e)}>
+          <button className="nav-link px-2 text-white" onClick={goFAQ}>
             FAQs
-          </a>
+          </button>
         </li>
         <li className="nav-item footLinks">
-          <a href="#" className="nav-link px-2 text-white" onClick={(e) => goAbout(navigate, e)}>
+          <button className="nav-link px-2 text-white" onClick={goAbout}>
             About
-          </a>
+          </button>
         </li>
       </ul>
     </footer>
